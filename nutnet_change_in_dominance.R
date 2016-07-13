bbc46e77e43425f476e9017e2a0e28c5fb84abef
klapierre <lapierre.kimberly@gmail.com>
2016-07-13 11:35:49 -0500

    Set up working space.

---
+++ nutnet_change_in_dominance.R
@@ -0,0 +1,8 @@
+library(lattice)
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+source('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\nutnet-cover-data.R')
+
+setwd('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\NutNet data')
