d714d62aee41fcbb8623bab91209fd5064d2dd99
klapierre <lapierre.kimberly@gmail.com>
2016-07-13 12:04:43 -0500

    Filter out dominant spp response ratios through time.

--- nutnet_change_in_dominance.R
+++ nutnet_change_in_dominance.R
@@ -6,3 +6,13 @@ library(tidyr)
 source('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\nutnet-cover-data.R')
 
 setwd('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\NutNet data')
+
+#calculate response ratio for change in spp cover
+coverRR <- trtCoverTime%>%
+  mutate(cover_RR=((rel_cover-pretrt_cover)/pretrt_cover))
+
+#filter out just the dominant species RR, as determined from pre-trt data
+domRR <- coverRR%>%
+  group_by(site_code, trt, plot, year)%>%
+  mutate(max=ifelse(pretrt_cover==max(pretrt_cover), 1, 0))%>%
+  filter(max==1)
