b087ef0ca5f2fc1b362d7b3889db5248e6c187df
klapierre <lapierre.kimberly@gmail.com>
2016-08-06 14:32:40 -0700

    Make biomass calculations their own script.

--- nutnet-basic-data-script.R
+++ nutnet-basic-data-script.R
@@ -7,38 +7,13 @@ setwd('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\NutNet data')
 
 
 ###read in data
-nutnetData <- read.csv('comb-by-plot-clim-soil-diversity21-Jun-2016.csv')
-
-nutnetBio <- nutnetData%>%
+nutnetData <- read.csv('comb-by-plot-clim-soil-diversity21-Jun-2016.csv')%>%
   #delete data with no cover
   filter(total_cover!='NULL')%>%
   #drop sites with just 3 plots
   filter(site_code!='ucsc.us', site_code!='elkh.us')%>%
   #get rid of observational only datasets
-  filter(experiment_type!='Observational')%>%
-  #get rid of exclosure plots
-  filter(Exclose==0)%>%
-  select(site_code, N, P, K, trt, plot, year_trt, year, rich, site_year_rich, MAT, MAP, RAIN_PET, plot_beta, total_mass, live_mass, dead_mass)
-
-
-###calculate ln RR from pre-treatment year
-#pull out pre-treatment data
-preTrtBio <- nutnetBio%>%filter(year_trt==0)%>%
-  select(site_code, plot, live_mass)
-names(preTrtBio)[names(preTrtBio)=="live_mass"] <- "yr0_live_mass"
-
-#pull out experimental data
-trtBio <- nutnetBio%>%filter(year_trt>0)%>%
-  #merge experimental years with pre-treatment years
-  left_join(preTrtBio, by=c('site_code', 'plot'))%>%
-  #remove sites without pretreatment data
-  filter(!is.na(yr0_live_mass), yr0_live_mass!='NULL')%>%
-  #remove plots without treatment year data
-  filter(!is.na(live_mass), live_mass!='NULL')%>%
-  #calculate change in ANPP from pre-treatment year to each treatment year
-  mutate(live_mass=as.numeric(live_mass), yr0_live_mass=as.numeric(yr0_live_mass))%>%
-  mutate(live_mass_lnRR=(log(live_mass/yr0_live_mass)))
-
+  filter(experiment_type!='Observational')
 
 
 #generate site information table
---
+++ nutnet_change_in_biomass.R
@@ -0,0 +1,29 @@
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+source('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\nutnet-basic-data-script.R')
+
+setwd('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\NutNet data')
+
+#pull out biomass data
+nutnetBio <- nutnetData%>%
+  select(site_code, N, P, K, Exclose, trt, plot, year_trt, year, rich, site_year_rich, MAT, MAP, RAIN_PET, plot_beta, total_mass, live_mass, dead_mass)
+
+###calculate biomass ln RR from pre-treatment year
+#pull out pre-treatment data
+preTrtBio <- nutnetBio%>%filter(year_trt==0)%>%
+  select(site_code, plot, live_mass)
+names(preTrtBio)[names(preTrtBio)=="live_mass"] <- "yr0_live_mass"
+
+#pull out experimental data
+trtBio <- nutnetBio%>%filter(year_trt>0)%>%
+  #merge experimental years with pre-treatment years
+  left_join(preTrtBio, by=c('site_code', 'plot'))%>%
+  #remove sites without pretreatment data
+  filter(!is.na(yr0_live_mass), yr0_live_mass!='NULL')%>%
+  #remove plots without treatment year data
+  filter(!is.na(live_mass), live_mass!='NULL')%>%
+  #calculate change in ANPP from pre-treatment year to each treatment year
+  mutate(live_mass=as.numeric(live_mass), yr0_live_mass=as.numeric(yr0_live_mass))%>%
+  mutate(live_mass_lnRR=(log(live_mass/yr0_live_mass)))
