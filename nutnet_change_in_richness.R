d80a3f9f5f61fd17ba4e8cadd0b4118b967a5365
klapierre <lapierre.kimberly@gmail.com>
2016-08-06 14:24:36 -0700

    Setting up workspace to calculate change in richness.

---
+++ nutnet_change_in_richness.R
@@ -0,0 +1,10 @@
+library(vegan)
+library(lattice)
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+source('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\nutnet-cover-data.R')
+
+setwd('C:\\Users\\Kim\\Dropbox\\NutNet\\NutNet-dominance\\NutNet data')
+
